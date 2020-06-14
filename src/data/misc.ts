8f311a60d962919afb92ff9cc34776b19c3cc49b
Ash <ashley@juice49.com>
2020-06-14 22:00:43 +0100

    Add misc external URLs

--- src/data/misc.ts
+++ src/data/misc.ts
@@ -1,21 +1,25 @@
 interface Misc {
   name?: string,
   slug: string,
-  description?: string
+  description?: string,
+  externalUrl?: string
 }
 
 const misc: Misc[] = [
   {
     name: '⏱ React useHistory hook',
-    slug: 'react-use-history-hook'
-  },
-  {
-    name: '🐝 Emoji Fizz Buzz',
-    slug: 'emoji-fizz-buzz'
+    slug: 'react-use-history-hook',
+    externalUrl: 'https://twitter.com/juice49/status/1060808585062559744'
   },
   {
     name: '💅 Custom multiline text underline with rounded caps',
-    slug: 'custom-multline-text-underline-with-rounded-caps'
+    slug: 'custom-multline-text-underline-with-rounded-caps',
+    externalUrl: 'https://codepen.io/Ash/pen/WBMxpp'
+  },
+  {
+    name: '🐝 Emoji Fizz Buzz',
+    slug: 'emoji-fizz-buzz',
+    externalUrl: 'https://codepen.io/Ash/pen/YXMMJq'
   }
 ]
 
