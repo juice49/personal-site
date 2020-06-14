8567e203174ae9111fccda002c6619a791f46e75
Ash <ashley@juice49.com>
2020-06-14 11:49:18 +0100

    Switch to `<StandardGrid />`

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -6,6 +6,7 @@ import Box from '../../components/box'
 import Stack from '../../components/stack'
 import Layout from '../../components/layout'
 import Heading from '../../components/heading'
+import StandardGrid, { StandardGridContent } from '../../components/standard-grid'
 import ArticleList, { ArticleListItem } from '../../components/article-list'
 
 const Page: NextPage = () => (
@@ -13,58 +14,48 @@ const Page: NextPage = () => (
     <Head>
       <title>Projects - Ash</title>
     </Head>
-    <Box
-      px={2}
-      mw={0}
-      css={`
-        margin-left: auto;
-        margin-right: auto;
-      `}
-    >
-      <ListBox>
-        <Heading as='h1'>
-          Projects
-        </Heading>
-        <ArticleList columns>
-          {projects.map(project => (
-            <ArticleListItem
-              key={project.slug}
-              heading={project.name}
-              description={project.description}
-              link={{
-                href: `/projects/${project.slug}`
-              }}
-            />
-          ))}
-        </ArticleList>
-      </ListBox>
-    </Box>
-    <Box
-      px={2}
-      mw={0}
-      css={`
-        margin-left: auto;
-        margin-right: auto;
-      `}
-    >
-      <ListBox>
-        <Heading as='h1'>
-          Misc
-        </Heading>
-        <ArticleList columns>
-          {misc.map(misc => (
-            <ArticleListItem
-              key={misc.slug}
-              heading={misc.name}
-              description={misc.description}
-              link={{
-                href: `/projects/${misc.slug}`
-              }}
-            />
-          ))}
-        </ArticleList>
-      </ListBox>
-    </Box>
+    <StandardGrid>
+      <StandardGridContent>
+        <ListBox>
+          <Heading as='h1'>
+            Projects
+          </Heading>
+          <ArticleList columns>
+            {projects.map(project => (
+              <ArticleListItem
+                key={project.slug}
+                heading={project.name}
+                description={project.description}
+                link={{
+                  href: `/projects/${project.slug}`
+                }}
+              />
+            ))}
+          </ArticleList>
+        </ListBox>
+      </StandardGridContent>
+    </StandardGrid>
+    <StandardGrid>
+      <StandardGridContent>
+        <ListBox>
+          <Heading as='h1'>
+            Misc
+          </Heading>
+          <ArticleList columns>
+            {misc.map(misc => (
+              <ArticleListItem
+                key={misc.slug}
+                heading={misc.name}
+                description={misc.description}
+                link={{
+                  href: `/projects/${misc.slug}`
+                }}
+              />
+            ))}
+          </ArticleList>
+        </ListBox>
+      </StandardGridContent>
+    </StandardGrid>
   </Layout>
 )
 
