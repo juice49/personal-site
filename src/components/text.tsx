8aefcc1a3daf79fdae7b84a636d9f1add176ae47
Ash <ashley@juice49.com>
2020-06-14 12:36:26 +0100

    Add monospace font stack

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -19,7 +19,7 @@ const Text = styled.span`
 
   ${variants({
     mono: `
-      font-family: Arnold;
+      font-family: Arnold, SFMono-Regular, Consolas, Liberation Mono, Menlo, monospace;
     `
   })}
 `
