01a8ff6a0a50357162b44ee9e9ff19df79ba7ac1
Ash <ashley@juice49.com>
2020-06-14 22:44:08 +0100

    Add Google Analytics (regerettably and temporarily)

---
+++ src/lib/gtag.ts
@@ -0,0 +1,17 @@
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = url => {
+  // @ts-ignore
+  window.gtag('config', process.env.GA_ID, {
+    page_path: url
+  })
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  // @ts-ignore
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value
+  })
+}
