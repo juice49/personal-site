1796cbc9b2280b6f3bedf8b384ddf058f6e732e6
Ash <ashley@juice49.com>
2021-07-25 22:55:57 +0100

    Add `NEXT_PUBLIC_OG_IMAGE_SERVICE_URL` environment variable

--- src/pages/this-is-my-jam/[id]/index.tsx
+++ src/pages/this-is-my-jam/[id]/index.tsx
@@ -95,7 +95,7 @@ export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
   return {
     notFound: typeof jam._id === 'undefined',
     props: {
-      ogImageUrl: `https://og-image.ash.gd/api/this-is-my-jam/${params.id}/og-image.png`,
+      ogImageUrl: `${process.env.NEXT_PUBLIC_OG_IMAGE_SERVICE_URL}/this-is-my-jam/${params.id}/og-image.png`,
       jam,
     },
     revalidate: 3600,
