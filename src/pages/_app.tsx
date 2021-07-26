2225fef2a153e4f11274205a34a93e519bd49361
Ash <ashley@juice49.com>
2021-07-26 14:41:59 +0100

    Add default OG image to all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import Providers from '../components/providers'
+import OgImageMeta from '../components/og-image-meta'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const title = 'Ash - full stack web developer'
@@ -13,6 +14,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <meta name='twitter:site' content='@juice49' />
         <meta key='og:title' property='og:title' content={title} />
       </Head>
+      <OgImageMeta title='I like to make things&mdash;usually with web technologies, and usually <em>for</em> the web.' />
       <Component {...pageProps} />
     </Providers>
   )
