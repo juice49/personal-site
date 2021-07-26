c627ab859c0a76adf80fead11addad03efa5029a
Ash <ashley@juice49.com>
2021-07-26 14:36:52 +0100

    Add `<OgImageMeta />` component for linking to dynamic OG image

---
+++ src/components/og-image-meta.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+interface Props {
+  title: string
+  date?: Date | string
+}
+
+const OgImageMeta: React.FC<Props> = ({ title, date }) => {
+  const params = new URLSearchParams({ title })
+
+  if (date) {
+    params.set('date', typeof date === 'string' ? date : date.toISOString())
+  }
+
+  const url = `${process.env.NEXT_PUBLIC_OG_IMAGE_SERVICE_URL}/og-image.png?${params}`
+
+  return (
+    <Head>
+      <meta key='og:image' property='og:image' content={url} />
+      <meta key='og:image:width' property='og:image:width' content='2048' />
+      <meta key='og:image:height' property='og:image:height' content='1260' />
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:image' content={url} />
+    </Head>
+  )
+}
+
+export default OgImageMeta
