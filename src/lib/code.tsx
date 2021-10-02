c7c4b68400cbfbbfa55ab10adbe03a1b319dd9df
Ash <ashley@juice49.com>
2021-10-02 14:53:33 +0100

    Fix typo

--- src/lib/code.tsx
+++ src/lib/code.tsx
@@ -9,7 +9,7 @@ import Code from '../components/code'
 // 1. Render the page on the server.
 // 2. During a server render, each code block is pushed to a `global`.
 // 3. When Next.js renders the page, each code block is rendered by Shiki.
-// 4. The Shiki output os passed to the Next.js page props.
+// 4. The Shiki output is passed to the Next.js page props.
 // 5. During a client render, React context is created based on the code block page props.
 // 6. During a client render, the code block component grabs the code block HTML from the context.
 
