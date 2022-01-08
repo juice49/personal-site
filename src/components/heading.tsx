bf3f2ef2bc8284a4ab55850f45fbe3fc33cd88b4
Ash <ashley@juice49.com>
2022-01-08 00:38:03 +0000

    Add automatic heading anchors and links to MDX documents

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -1,6 +1,7 @@
 import { styled } from '../stitches.config'
 
 const Heading = styled('h1', {
+  position: 'relative',
   fontSize: '1.4rem',
   fontFamily: 'Zangezi Sans',
   lineHeight: 1.2,
@@ -13,6 +14,29 @@ const Heading = styled('h1', {
         textAlign: 'center',
       },
     },
+  },
+  '&::before': {
+    position: 'absolute',
+    inset: 'calc($small * -1) calc($medium * -1)',
+    content: '',
+  },
+  '.heading-anchor-link': {
+    position: 'absolute',
+    transform: 'translateX(calc((100% + $space$small) * -1))',
+    opacity: 0,
+    transition: 'opacity 180ms',
+    '&::before': {
+      position: 'absolute',
+      zIndex: -1,
+      inset: '-0.1em -0.3em',
+      backgroundColor: 'rgb(255 255 255 / 0.6)',
+      borderRadius: '0.25em',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      content: '',
+    },
+  },
+  '&:hover .heading-anchor-link': {
+    opacity: 1,
   },
 })
 
