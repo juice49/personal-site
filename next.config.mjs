f0467374e651c168283deb1c36054b2b0b25c0bd
Ash <ashley@juice49.com>
2022-10-01 22:43:00 +0100

    Switch to `remotePatterns` for configuring permitted Next image sources

--- next.config.mjs
+++ next.config.mjs
@@ -88,13 +88,15 @@ function createNextStaticProps(map) {
 export default withMdx({
   pageExtensions: ['ts', 'tsx', 'mdx'],
   images: {
-    domains: [
-      'cdn.sanity.io',
-      'is1-ssl.mzstatic.com',
-      'is2-ssl.mzstatic.com',
-      'is3-ssl.mzstatic.com',
-      'is4-ssl.mzstatic.com',
-      'is5-ssl.mzstatic.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.mzstatic.com',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
   },
